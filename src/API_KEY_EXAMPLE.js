cdfc627b9f8e80f80abaf37abb5894554945194f
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-06-25 12:35:40 -0400

    Setup OpenWeather API request, and logged response to the console

---
+++ src/API_KEY_EXAMPLE.js
@@ -0,0 +1,2 @@
+// replace 'xxx' with your api_key and rename the file to 'API_KEY.js'
+module.exports = 'xxx'
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import API_KEY from './API_KEY';
 
 const Root = styled.div`
   display: flex;
@@ -9,6 +11,19 @@ const Root = styled.div`
 
 class App extends Component {
   render() {
+    console.log('searching for Toronto');
+    axios.get('https://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        q: 'Toronto',
+        APPID: API_KEY
+      }
+    })
+    .then((response)=>{
+      console.log('OpenWeatherAPI response.data =', response.data);
+    })
+    .catch((error)=>{
+      console.log('Error fetching data from OpenWeatherAPI:', error);
+    }) 
     return <Root />
   }
 }
